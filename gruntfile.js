7d5893ead9b4dd090924442a6e31ae17d4172afa
Toby <tobysoff@hotmail.com>
2014-09-19 11:11:21 +1200

    adds files and folders

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        compass: {
+          dev: {
+            options: {
+              config: 'config.rb',
+              force: true
+            }
+          }
+        },
+
+        autoprefixer: {
+          options: {
+            browsers: ['last 2 versions']
+          },
+          dist: {
+            files: [{
+              expand: true,
+              cwd:    'css/',
+              src:    '{,*/}*.css',
+              dest:   'css/'
+            }]
+          }
+        },
+
+        cssmin: {
+          my_target: {
+            files: [{
+              expand: true,
+              cwd:    'css/',
+              src:   ['*.css', '!*.min.css'],
+              dest:   'css/',
+              ext:    '.min.css'
+            }]
+          }
+        },
+
+        watch: {
+          compass: {
+            files: ['scss/*.scss', 'scss/**/*.scss', 'scss/**/**/*.scss', 'scss/**/**/**/*.scss'],
+            tasks: ['compass:dev', 'autoprefixer', 'cssmin']
+          }
+        }
+
+    }); // close initConfig
+
+  // default tasks
+  grunt.registerTask('default', 'watch');
+
+};
