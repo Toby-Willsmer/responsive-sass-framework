8e39c31b9aa148e8acce16225beac14b4a006c33
Toby Willsmer <toby.willsmer@nzpost.co.nz>
2017-04-05 13:16:38 +1200

    re adds the missing config addition and resets the base settings file to previous commit

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-
+add_import_path "scss/base-settings"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -7,7 +7,7 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
-fonts_dir = "fonts"
+# fonts_dir = "fonts"
 
 output_style = :nested
 environment = :development
